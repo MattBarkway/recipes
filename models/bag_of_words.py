d8bd36bdd5fac515cc4f22f7799fc4537b7b8b27
MattBarkway <33966203+MattBarkway@users.noreply.github.com>
2020-03-07 17:22:06 +0000

    cleanup of corpus classes

--- models/bag_of_words.py
+++ models/bag_of_words.py
@@ -1,9 +1,11 @@
 """
 Word2Vec model for recipe analysis
 """
+import itertools
 import json
 import os
 import csv
+import random
 import re
 from collections import defaultdict
 
@@ -14,6 +16,7 @@ from gensim.models import Word2Vec, KeyedVectors
 from sklearn.cluster import KMeans
 from scipy.spatial import KDTree
 
+from recipes.utils.exceptions import SetupError
 from recipes.utils.helpers import get_gen_at_index
 
 
@@ -22,16 +25,20 @@ class RecipeAnalyzer(object):
     Class to inspect and analyze the recipe data
     """
 
-    def __init__(self):
-        self.corpus = RecipeCorpus()
-        self._recipe_vectors = {}
+    def __init__(self, corpus, num_features=100):
+        self.corpus = corpus
+        self.recipe_vectors = {}
         self._ingredient_vectors = {}
+        self.num_features = num_features
         self._model = None
         self._k_means = None
         self._vectors = None
 
     def init(self):
-        pass
+        self.get_or_create_model()
+        self.save_template()
+        self.load_or_train()
+        self.save_trained()
 
     @property
     def model(self):
@@ -42,9 +49,9 @@ class RecipeAnalyzer(object):
         Attempt to load an existing model from path, else create one
         """
         if not path:
-            path = os.path.join('.', 'created_models', 'w2v_ingredients.model')
+            path = os.path.join('..', 'created_models', 'w2v_ingredients.model')
         if not os.path.exists(path):
-            self._model = Word2Vec([x for x in self.corpus.get_processed_ingredients()])
+            self._model = Word2Vec([x for _, x in self.corpus], size=self.num_features)
         else:
             self._model = Word2Vec.load(path)
 
@@ -52,26 +59,26 @@ class RecipeAnalyzer(object):
         """
         Save the Word2Vec model with a name
         """
-        self._model.save(os.path.join('.', 'created_models', name))
+        self._model.save(os.path.join('..', 'created_models', name))
 
     def save_trained(self, name='trained_w2v_ingredients.kv'):
         """
         Save the word vectors to a .kv file with name
         """
         if not self._vectors:
-            raise SetupError('Model not yet trained')
-        self._vectors.save(os.path.join('.', 'created_models', name))
+            raise SetupError('Model has not yet been trained')
+        self._vectors.save(os.path.join('..', 'created_models', name))
 
     def load_or_train(self, name='trained_w2v_ingredients.kv'):
         """
         Attempt to load the specified .kv file, else train model and create one
         """
-        path = os.path.join('.', 'created_models', name)
+        path = os.path.join('..', 'created_models', name)
         if not os.path.exists(path):
             self.train()
         else:
             self._vectors = KeyedVectors.load(path, mmap='r')
-            self._recipe_vectors = self.calc_recipe_vectors()
+            self.recipe_vectors = self.calc_recipe_vectors()
 
     def train(self, epochs=5, **kwargs):
         """
@@ -80,24 +87,27 @@ class RecipeAnalyzer(object):
         self._model.train(self.corpus.get_processed_ingredients(), epochs=epochs,
                           total_examples=self._model.corpus_count, **kwargs)
         self._vectors = self._model.wv
-        self._recipe_vectors = self.calc_recipe_vectors()
+        self.recipe_vectors = self.calc_recipe_vectors()
 
     def calc_recipe_vectors(self):
         """
         Calculate vectors for each recipe, by summing the individual word vectors for each ingredient
         """
         vector_dict = {}
-        for idx, (name, processed_ingredients) in enumerate(self.corpus.get_ingredient_tuples()):
+        for idx, (name, processed_ingredients) in enumerate(self.corpus):
             vecs = []
-            for word in processed_ingredients:
-                try:
-                    vec = self._vectors[word]
-                except KeyError:
-                    vec = np.zeros(100)
-                vecs.append(vec)
+            for ingredient_item in processed_ingredients:
+                for word in ingredient_item.split(' '):
+                    if not word:
+                        continue
+                    try:
+                        vec = self._vectors[word]
+                    except KeyError:
+                        vec = np.zeros(self.num_features)
+                    vecs.append(vec)
             vec_sum = sum(vecs)
-            if not isinstance(vec_sum, np.ndarray) or len(vec_sum) != 100:
-                vec_sum = np.zeros(100)
+            if not isinstance(vec_sum, np.ndarray) or len(vec_sum) != self.num_features:
+                vec_sum = np.zeros(self.num_features)
             vector_dict[name] = vec_sum
         return vector_dict
 
@@ -105,22 +115,28 @@ class RecipeAnalyzer(object):
         """
         Calculate vectors for each ingredient item
         """
-        ingredients = self.corpus.get_flat_ingredients()
-        unique_ingredients = set(list(ingredients))
+        unique_ingredients = set()
+        for _, ingredients in self.corpus:
+            for ingredient in ingredients:
+                unique_ingredients.add(ingredient)
+        unique_ingredients = list(unique_ingredients)
+        print(f'corpus contains {len(unique_ingredients)} unique words')
         vector_dict = {}
         for ingredient in unique_ingredients:
             if not ingredient:
                 continue
             vecs = []
             for word in ingredient.split(' '):
+                if not word:
+                    continue
                 try:
                     vec = self._vectors[word]
                 except KeyError:
-                    vec = np.zeros(100)
+                    vec = np.zeros(self.num_features)
                 vecs.append(vec)
             vec_sum = sum(vecs)
-            if not isinstance(vec_sum, np.ndarray) or len(vec_sum) != 100:
-                vec_sum = np.zeros(100)
+            if not isinstance(vec_sum, np.ndarray) or len(vec_sum) != self.num_features:
+                vec_sum = np.zeros(self.num_features)
             vector_dict[ingredient] = vec_sum
         return vector_dict
 
@@ -130,7 +146,7 @@ class RecipeAnalyzer(object):
         :return:
         """
         if not path:
-            path = os.path.join('.', 'created_models', 'all_vectors.json')
+            path = os.path.join('..', 'created_models', 'all_vectors.json')
         ingredient_vecs = self.calc_ingredient_vecs()
         recipe_vecs = self.calc_recipe_vectors()
         word_vecs = {
@@ -147,9 +163,9 @@ class RecipeAnalyzer(object):
 
     def load_vecs(self, path=None):
         if not path:
-            os.path.join('.', 'created_models', 'all_vectors.json')
+            os.path.join('..', 'created_models', 'all_vectors.json')
         data = json.load(path)
-        self._recipe_vectors = data['recipes']
+        self.recipe_vectors = data['recipes']
         self._ingredient_vectors = data['ingredients']
         self._vectors = data['words']
 
@@ -159,32 +175,33 @@ class RecipeAnalyzer(object):
         """
         self._k_means = KMeans(
             n_clusters=n, random_state=0
-        ).fit(np.asarray(self._recipe_vectors.values()))
+        ).fit(np.asarray(self.recipe_vectors.values()))
 
     def print_clusters(self):
         """
         Print the K-means cluster data
         """
-        labels = list(self._k_means.predict(np.asarray(self._recipe_vectors.values())))
+        labels = list(self._k_means.predict(np.asarray(self.recipe_vectors.values())))
         labels_dict = defaultdict(list)
         for idx, label in enumerate(labels):
             labels_dict[label].append(get_gen_at_index(self.corpus.get_names(), idx))
         print(labels_dict)
 
-    def get_similar_to(self, recipe_name, k=5):
+    def get_similar_to(self, name, k=5):
         """
         Get the name of the k most similar recipes to the inputted name.
         Using cosine distance
         """
-        target_vec = self._recipe_vectors[recipe_name]
-        reduced_rec_vectors = {
-            key: self._recipe_vectors[key] for key in self._recipe_vectors.keys() if key != recipe_name
-        }
-        kd_tree = KDTree(np.asarray(reduced_rec_vectors.values()))
+        target_vec = self.recipe_vectors[name]
+        reduced_rec_vectors = [
+            self.recipe_vectors[key] for key in self.recipe_vectors.keys() if key != name
+        ]
+        kd_tree = KDTree(reduced_rec_vectors)
         _, indexes = kd_tree.query(target_vec, k=k)
-        similar = [get_gen_at_index(self.corpus.get_names(), idx) for idx in indexes]
+        # avoid get gen at index, very slow
+        similar = [get_gen_at_index(self.corpus, idx)[0] for idx in indexes]
         similar_str = '\n- '.join(similar)
-        print(f'Top {k} most similar recipes to {recipe_name}: \n- {similar_str}')
+        print(f'Top {k} most similar recipes to {name}: \n- {similar_str}')
         return similar
 
     def replace_ingredient(self, missing_ingredient, k=5):
@@ -197,16 +214,23 @@ class RecipeAnalyzer(object):
         return replacements
 
 
-class RecipeCorpus(object):
+class BaseCorpus(object):
+    def __init__(self, corpus_path=None):
+        self.corpus_path = corpus_path
+
+    def __iter__(self):
+        raise NotImplementedError('No defined method for iteration')
+
+
+class BaseRecipeCorpus(BaseCorpus):
     """
     Iterator that yields recipe data
     """
 
     def __init__(self, corpus_path=None):
         if not corpus_path:
-            corpus_path = os.path.join('.', 'data', 'bbc', 'recipe_details.csv')
-        self.corpus_path = corpus_path
-        self._names = []
+            corpus_path = os.path.join('..', 'data', 'bbc', 'recipe_details.csv')
+        super().__init__(corpus_path=corpus_path)
         self.column_names = []
 
     def __iter__(self):
@@ -214,47 +238,10 @@ class RecipeCorpus(object):
             csv_reader = csv.reader(f)
             self.column_names = next(csv_reader)
             for line in csv_reader:
-                yield line
-
-    def get_row(self, index):
-        with open(self.corpus_path, 'r', encoding='utf-8') as f:
-            csv_reader = csv.reader(f)
-            return list(csv_reader)[index+1]
-
-    def get_col_values(self, col_indexes):
-        for line in self.__iter__():
-            yield [line[col_index] for col_index in col_indexes]
-
-    def get_col_value(self, col_index, index):
-        return self.get_row(index)[col_index]
-
-    def get_ingredient_tuples(self):
-        for name, ingredients in self.get_col_values([1, 5]):
-            formatted_ingredient_items = [self.pre_process_ingredient(ingredient)
-                                          for ingredient in ingredients.split('::')]
-            yield name, utils.simple_preprocess(' '.join(formatted_ingredient_items))
-
-    def get_processed_ingredients(self):
-        for ingredients in self.get_col_values([5]):
-            formatted_ingredient_items = [self.pre_process_ingredient(ingredient)
-                                          for ingredient in ingredients[0].split('::')]
-            yield utils.simple_preprocess(' '.join(formatted_ingredient_items))
-
-    def get_names(self):
-        return (x[0] for x in self.get_col_values([1]))
-
-    def get_name(self, index):
-        return self.get_col_value(1, index)
-
-    def get_flat_ingredients(self):
-        for line in csv.reader(open(self.corpus_path, 'r', encoding='utf-8'), delimiter=','):
-            if not line[0]:
-                continue  # skip header row
-            for ingredient in line[5].split('::'):
-                yield self.pre_process_ingredient(ingredient)
+                yield tuple(line)
 
     @staticmethod
-    def pre_process_ingredient(item):
+    def remove_unwanted_chars(item):
         """
         Remove quantities from ingredient items.
         e.g:
@@ -271,14 +258,103 @@ class RecipeCorpus(object):
         ]
         return re.sub(r'|'.join(char_sets), '', item).strip()
 
+    # def get_row(self, index):
+    #     with open(self.corpus_path, 'r', encoding='utf-8') as f:
+    #         csv_reader = csv.reader(f)
+    #         return list(csv_reader)[index+1]
+    #
+    # # All pre-processing should be done in the model
+    # def get_col_values(self, col_indexes):
+    #     for line in self.__iter__():
+    #         yield [line[col_index] for col_index in col_indexes]
+    #
+    # def get_col_value(self, col_index, index):
+    #     return self.get_row(index)[col_index]
+    #
+    # def get_ingredient_tuples(self):
+    #     for name, ingredients in self.get_col_values([1, 5]):
+    #         formatted_ingredient_items = [self.pre_process_ingredient(ingredient)
+    #     #                                       for ingredient in ingredients.split('::')]
+    #         yield name, utils.simple_preprocess(' '.join(formatted_ingredient_items))
+    #
+    # def get_processed_ingredients(self):
+    #     for ingredients in self.get_col_values([5]):
+    #         formatted_ingredient_items = [self.pre_process_ingredient(ingredient)
+    #                                       for ingredient in ingredients[0].split('::')]
+    #         yield utils.simple_preprocess(' '.join(formatted_ingredient_items))
+    #
+    # def get_names(self):
+    #     return (x[0] for x in self.get_col_values([1]))
+    #
+    # def get_flat_ingredients(self):
+    #     for line in csv.reader(open(self.corpus_path, 'r', encoding='utf-8'), delimiter=','):
+    #         if not line[0]:
+    #             continue  # skip header row
+    #         for ingredient in line[5].split('::'):
+    #             yield self.pre_process_ingredient(ingredient)
+    #
+    # @staticmethod
+    # def pre_process_ingredient(item):
+    #     """
+    #     Remove quantities from ingredient items.
+    #     e.g:
+    #         - '100g/3½oz carrot, grated, peeled' -> 'carrot grated peeled'
+    #         - '600g/1lb 5oz fresh apricots' -> 'fresh apricots'
+    #     :param item:
+    #     :return:
+    #     """
+    #     char_sets = [
+    #         r'\d+[a-zA-Z/¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞]+',
+    #         r'\d+',
+    #         r'[().,!?\'"\-+]',
+    #         r'[¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞]',
+    #     ]
+    #     return re.sub(r'|'.join(char_sets), '', item).strip()
 
-class SetupError(BaseException):
-    pass
+
+class IngredientOnlyCorpus(BaseRecipeCorpus):
+    def __init__(self, corpus_path=None):
+        super().__init__(corpus_path=corpus_path)
+
+    def __iter__(self):
+        """
+        Select only name and ingredients list from corpus
+        :return:
+        """
+        for row_tuple in super(IngredientOnlyCorpus, self).__iter__():
+            ingredients = row_tuple[5]
+            ingredients = self.pre_process_ingredients(ingredients)
+            yield row_tuple[1], ingredients
+
+    def pre_process_ingredients(self, ingredients):
+        return [self.remove_unwanted_chars(ingredient) for ingredient in ingredients.split('::')]
+
+
+class TitleOnlyCorpus(BaseRecipeCorpus):
+    def __init__(self, corpus_path=None):
+        super().__init__(corpus_path=corpus_path)
+
+    def __iter__(self):
+        """
+        Select only name and ingredients list from corpus
+        :return:
+        """
+        for row_tuple in super(TitleOnlyCorpus, self).__iter__():
+            full_name = row_tuple[1]
+            processed_name = self.pre_process_name(full_name)
+            yield full_name, processed_name
+
+    def pre_process_name(self, name):
+        return self.remove_unwanted_chars(name)
 
 
 if __name__ == "__main__":
-    _model = RecipeAnalyzer()
-    _model.get_or_create_model()
-    _model.save_template()
-    _model.load_or_train()
-    _model.save_trained()
+    _model = RecipeAnalyzer(corpus=IngredientOnlyCorpus())
+    _model.init()
+    for i in range(5):
+        _model.get_similar_to(random.choice(list(_model.recipe_vectors.keys())))
+# TODO:
+# model loads generic corpus:
+# should have one parent corpus with __iter__ that reads entire line,
+# then some subclasses which only return name and ingredients
+# for each item in corpus
--- run_models.py
+++ run_models.py
@@ -1,17 +1,16 @@
 """
 Run the models
 """
-from recipes.models.bag_of_words import RecipeAnalyzer
+from recipes.models.bag_of_words import RecipeAnalyzer, IngredientOnlyCorpus
 
 
 def run():
-    rec_analyser = RecipeAnalyzer()
+    rec_analyser = RecipeAnalyzer(IngredientOnlyCorpus)
     rec_analyser.get_or_create_model()
     rec_analyser.save_template()
     rec_analyser.load_or_train()
     rec_analyser.save_trained()
     rec_analyser.save_vecs()
-    # TODO need to split classes to have one for W2V model, then one for data analysis
 
 
 if __name__ == "__main__":
