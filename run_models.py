47be2f822a3c92bdbc3dffd1999bfb155991e2cc
MattBarkway <33966203+MattBarkway@users.noreply.github.com>
2020-03-10 14:04:43 +0000

    Switch to using cKDTree

--- run_models.py
+++ run_models.py
@@ -11,7 +11,7 @@ def run():
     rec_analyser = RecipeAnalyzer(IngredientOnlyCorpus(corpus_path=os.path.join('data', 'recipe_details.csv')))
     rec_analyser.init('w2v_ingredients.model', 'trained_w2v_ingredients.kv')
     for i in range(5):
-        rec_analyser.get_similar_to(random.choice(list(rec_analyser.recipe_vectors.keys())))
+        rec_analyser.get_similar_recipes(random.choice(list(rec_analyser.recipe_vectors.keys())))
     for i in range(5):
         rec_analyser.replace_ingredient(random.choice(list(rec_analyser.ingredient_vectors.keys())))
 
